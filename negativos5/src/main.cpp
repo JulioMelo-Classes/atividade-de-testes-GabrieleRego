5db80f787e8f4734a16b1c599718c654cfe2d119
GabrieleRego <102765119+GabrieleRego@users.noreply.github.com>
2022-03-31 17:44:33 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -7,7 +7,7 @@ const int SIZE = 5; // input size.
 int main(void)
 {
     int num, count=0;
-   for(int i =0; i<5;i++){
+   for(int i =0; i<SIZE;i++){
         cin>>num;
        if(num <0) count++;
    }
